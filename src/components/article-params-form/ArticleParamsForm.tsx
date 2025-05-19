9cfd13f0aabc5a31458876a3abc99d7e114ad93e
mylittletraf <kanviser@gmail.com>
2025-05-20 01:03:58 +0300

    create form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,134 @@
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import {
+	ArticleStateType,
+	defaultArticleState,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (props: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const [isMenuOpen, setMenu] = useState(false);
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleState.contentWidth);
+
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	useOutsideClickClose({
+		isOpen: isMenuOpen,
+		rootRef: ref,
+		onClose: () => setMenu(false),
+		onChange: setMenu,
+	});
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setArticleState({
+			...articleState,
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor,
+			backgroundColor,
+			contentWidth,
+		});
+	};
+
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={ref}>
+			<ArrowButton onClick={() => setMenu(!isMenuOpen)} isOpen={isMenuOpen} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isMenuOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Select
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						onChange={setFontFamily}
+					/>
+
+					<RadioGroup
+						selected={fontSize}
+						options={fontSizeOptions}
+						name='fontSize'
+						title='Размер шрифта'
+						onChange={setFontSize}
+					/>
+
+					<Select
+						selected={fontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+						onChange={setFontColor}
+					/>
+
+					<Separator />
+
+					<Select
+						selected={backgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+						onChange={setBackgroundColor}
+					/>
+
+					<Select
+						selected={contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						onChange={setContentWidth}
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button title='Сбросить' type='clear' htmlType='reset' />
+						<Button title='Применить' type='apply' htmlType='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
